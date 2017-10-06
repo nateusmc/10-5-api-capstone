12fa321ea8f23507b437ba7ef2bb29578a27c05a
Nate Garrett <nate.garrett1992@gmail.com>
2017-10-06 12:43:50 -0500

    added features to work on next comment at bottom of JS file, ready to merge with adam branch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const key = 'ZdDgzt8HJYZHA41JfAlYJMA3OAMMh0VV';
 const searchEndPoint = 'https://api.giphy.com/v1/gifs/search?';
 const translateEndPoint = 'https://api.giphy.com/v1/gifs/translate?api_key=ZdDgzt8HJYZHA41JfAlYJMA3OAMMh0VV&s=';
-const trendingEndPoint = 'https://api.giphy.com/v1/gifs/trending?api_key=ZdDgzt8HJYZHA41JfAlYJMA3OAMMh0VV&limit=25&rating=G';
+const trendingEndPoint = 'https://api.giphy.com/v1/gifs/trending?api_key=ZdDgzt8HJYZHA41JfAlYJMA3OAMMh0VV&limit=10&rating=G';
 const randomEndPoint = 'https://api.giphy.com/v1/gifs/random?api_key=ZdDgzt8HJYZHA41JfAlYJMA3OAMMh0VV&tag=&rating=G';
 const getByIdEndPoint = 'https://api.giphy.com/v1/gifs/?api_key=ZdDgzt8HJYZHA41JfAlYJMA3OAMMh0VV';
 const getByIdSeperatedCommasEndPoint = 'https://api.giphy.com/v1/gifs?api_key=ZdDgzt8HJYZHA41JfAlYJMA3OAMMh0VV&gif_ids=';
@@ -43,50 +43,16 @@ function renderGiffs(/*arr*/) {
     $('.js-results').append(`<img src="${STORE.giffs/*arr*/[i].images.original.url}"/>`);
   }
 
+/*
 
+Features:
 
-// function getDataFromApi(searchTerm,) {
-//     // const params = {
-        
-//     //   q: searchTerm,
-//     //   api_key: key
-//     // };
-//     $.getJSON(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${searchTerm}`, (response) => {
-//     //   Objects for our rendering functions
-           
-       
-//         console.log(response)
-//     });
-//     // insert render function
-//   }
-//   getDataFromApi('dogs');
+-10 pics per page and scroll down and continue to populate the page below bottom content
 
+-onMouseOver on a GIF; it will increase in size.
 
+-onRightClick drop down menu pops out with options to 'save', 'share', etc.
 
+-Hot topics section off to the side that links to trending categories.
 
-
-// function getGiffApis() {
-//     // get giffs from api
-//     //  create callback function
-
-// }
-  
-
-  // render giffs from giffs array in STORE    
-  // render landing page
-  // render searched giffs
-  // auto render trending giffs
-  // auto render popular #'tags
-}
-
-// function eventListeners () {
-// search field listener on form
-// recover search fild data value
-// click on trending giffs to expand
-// click on #tags and run search
-//     // expand giffs returned from search
-// }
-
-
-
- 
+*/
